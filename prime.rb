6a1a566fe884ebec28ce03850091695ae6716c39
Jalysa Reason <jalysareason@gmail.com>
2019-09-22 12:54:10 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,20 @@
+def prime?
+  it 'returns true for prime numbers' do
+    puts(prime?(2))=(true)
+    puts(prime?(3))=(true)
+    puts(prime?(11))=(true)
+    puts(prime?(105557))=(true)
+  end
+
+def non-prime
+  it 'returns false for non-prime numbers' do
+    puts(prime?(-1))=(false), "Be sure to account for negative numbers!"
+    puts(prime?(0))=(false)
+    puts(prime?(1))=(false)
+    puts(prime?(4))=(false)
+    puts(prime?(40))=(false)
+    puts(prime?(1763))=(false)
+    puts(prime?(101013))=(false)
+  end
+end
 
